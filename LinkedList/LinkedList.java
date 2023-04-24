e8939b8478dd0e62f2a89a60a71bbe9809afa303
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-04-24 17:00:05 +0300

    Reverse between given indexes

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -279,4 +279,26 @@ public class LinkedList {
         }
         return slow;
     }
+    public void reverseBetween(int m, int n) {
+        if (head == null) {
+            return;
+        }
+
+        Node dummy = new Node(0);
+        dummy.next = head;
+        Node prev = dummy;
+
+        for (int i = 0; i < m; i++) {
+            prev = prev.next;
+        }
+
+        Node current = prev.next;
+        for (int i = 0; i < n - m; i++) {
+            Node temp = current.next;
+            current.next = temp.next;
+            temp.next = prev.next;
+            prev.next = temp;
+        }
+        head = dummy.next;
+    }
 }
